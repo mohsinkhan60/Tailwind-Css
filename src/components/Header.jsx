ff60c0c3caf19087ee084962f78e090994e22101
mohsinkhan60 <mohsinsaffy@gmail.com>
2024-08-14 23:44:45 +0500

    add Header Section

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,19 @@
-
-
 export const Header = () => {
   return (
-    <div>Header</div>
-  )
-}
-export default Header
+    <div>
+      <header className="w-full h-auto">
+        <img
+          className="hidden w-full md:block"
+          src="https://cdn.pwskills.com/assets/uploads/banners/63a18cca807628f5968f528a/web.jpg"
+          alt="img"
+        />
+        <img
+          className="w-full md:hidden"
+          src="https://cdn.pwskills.com/assets/uploads/banners/63a18cca807628f5968f528a/mobile.jpg"
+          alt="img"
+        />
+      </header>
+    </div>
+  );
+};
+export default Header;
